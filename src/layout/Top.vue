01c7529138292927f6236e2920709b2fb512fb99
wsq <wangsq0926@163.com>
2021-07-02 11:19:44 +0800

    workflow test

--- src/layout/Top.vue
+++ src/layout/Top.vue
@@ -5,7 +5,7 @@
             <div class="top_right">
                 <el-dropdown trigger="click">
                     <span class="el-dropdown-link">
-                        用户名
+                        wsq
                         <i class="el-icon-arrow-down el-icon--right"></i>
                     </span>
                     <el-dropdown-menu slot="dropdown">
