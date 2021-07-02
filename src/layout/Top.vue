cb3f1aac22dba334b7bc9e04f55ccea92c3efc5b
wsq <wangsq0926@163.com>
2021-07-02 14:41:19 +0800

    workflow test

--- src/layout/Top.vue
+++ src/layout/Top.vue
@@ -5,8 +5,7 @@
             <div class="top_right">
                 <el-dropdown trigger="click">
                     <span class="el-dropdown-link">
-                        wsqww
-                        <i class="el-icon-arrow-down el-icon--right"></i>
+                        WSQ <i class="el-icon-arrow-down el-icon--right"></i>
                     </span>
                     <el-dropdown-menu slot="dropdown">
                         <el-dropdown-item
